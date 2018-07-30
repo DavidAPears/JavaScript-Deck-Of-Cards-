3a99d532437138f0a08537d8c9a41cb5975d194c
David A Pears <davidapears@gmail.com>
2018-07-30 21:07:17 +0100

    Methods added to SHUFFLE, DEAL and RESET cards

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -1,7 +1,46 @@
+
+// // Object Destructuring to start the 'shuffle' method:
+// // Array Destructuring to swap 'i' & 'm' before returning 'this'
+//
+//   shuffle(){
+//     const { deck } = this;
+//     let m = deck.length, i;
+//
+//     while(m){
+//       i = Math.floor(Math.random() * m--);
+//
+//       [deck[m], deck[i]] = [deck[i], deck[m]];
+//     }
+//
+//     return this;
+//   }
+//
+// // Deal method that returns one card and removes it from the deck
+//   deal(){
+//     return this.deck.pop();
+//   }
+// }
+//
+// // New instance of deck, call shuffle on it then deal:
+// const deck1 = new Deck();
+// deck1.shuffle()
+// // console.log(deck1.deck);
+// deck1.deal()
+// // console.log(deck1.deck);
+// console.log(deal());
+
+
 // JavaScript Deck Of Cards:
 
-class Deck {
-  constructor() {
+
+class Deck{
+  constructor(){
+    this.deck = [];
+    this.reset();
+    this.shuffle();
+  }
+
+  reset(){
     this.deck = [];
 
     const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
@@ -13,7 +52,33 @@ class Deck {
       }
     }
   }
+
+// Object Destructuring to start the 'shuffle' method:
+// Array Destructuring to swap 'i' & 'm' before returning 'this'
+
+  shuffle(){
+    const { deck } = this;
+    let m = deck.length, i;
+
+    while(m){
+      i = Math.floor(Math.random() * m--);
+
+      [deck[m], deck[i]] = [deck[i], deck[m]];
+    }
+
+    return this;
+  }
+
+// Deal method that returns one card and removes it from the deck
+  deal(){
+    return this.deck.pop();
+  }
+  // console.log  TODO: How do I print this single card?
 }
 
 const deck1 = new Deck();
 console.log(deck1.deck);
+// output: shuffled deck
+deck1.reset();
+console.log(deck1.deck);
+// output: reset deck
